907be7b3aac03e8b941c61393a3e63ee62110df8
codd1e <andreinik32@gmail.com>
2022-10-22 21:58:29 +0300

    last stage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, HashRouter, Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -11,20 +11,20 @@ import Recipe from "./pages/Recipe";
 function App() {
   return (
       <>
-        <Router basename="/react-food">
-            <Header/>
-            <main className="container content">
-                <Routes>
-                    <Route exact path='/' element={<Home/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/contact' element={<Contact/>}/>
-                    <Route path='/category/:name' element={<Category/>}/>
-                    <Route path='/meal/:id' element={<Recipe/>}/>
-                    <Route path='*' element={<NotFound/>}/>
-                </Routes>
-            </main>
-            <Footer/>
-        </Router>
+          <Router>
+              <Header/>
+              <main className="container content">
+                  <Routes>
+                      <Route exact path='/' element={<Home/>}/>
+                      <Route path='/about' element={<About/>}/>
+                      <Route path='/contact' element={<Contact/>}/>
+                      <Route path='/category/:name' element={<Category/>}/>
+                      <Route path='/meal/:id' element={<Recipe/>}/>
+                      <Route path='*' element={<NotFound/>}/>
+                  </Routes>
+              </main>
+              <Footer/>
+          </Router>
       </>
   )
 }
