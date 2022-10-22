312a36a81245693c507e8b68d38b1ff253883019
codd1e <andreinik32@gmail.com>
2022-10-22 22:13:41 +0300

    change directory

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Recipe from "./pages/Recipe";
 function App() {
   return (
       <>
-          <Router basename="/react-food">
+          <Router>
               <Header/>
               <main className="container content">
                   <Routes>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <HashRouter>
+          <App />
+      </HashRouter>
   </React.StrictMode>
 );
