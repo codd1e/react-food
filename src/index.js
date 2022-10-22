e04d1179d0b63ee339db8c6a7a42ae1db6678b90
codd1e <andreinik32@gmail.com>
2022-10-22 22:05:26 +0300

    change homepage

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import {HashRouter} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-        <App />
-    </HashRouter>
+      <App />
   </React.StrictMode>
 );
