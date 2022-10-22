ce59e754a0c07ce5dbf4664a0b09bf52c3055b24
codd1e <andreinik32@gmail.com>
2022-10-22 22:28:24 +0300

    changed icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header () {
     return(
         <nav className="green darken-1">
             <div className="nav-wrapper">
-                <Link to="/" className="brand-logo">React-Food</Link>
+                <Link to="/" className="brand-logo">React Food</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
